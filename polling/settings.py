a75fd2b0d82415f055e8ad6fdac496bbf8aeb3c6
sharmahardik <hardiksharma66@gmail.com>
2022-02-06 11:19:00 +0000

    updated test dictionary in DATABASES (settings.py)

--- polling/settings.py
+++ polling/settings.py
@@ -85,6 +85,9 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': '',
+        'TEST': {
+            'NAME': 'config(TEST_NAME)',
+            }
 
     }
 }
